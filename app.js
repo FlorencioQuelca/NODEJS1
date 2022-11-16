13c4f058db6d9ac6606701c905c61c63fcb04e1a
FlorencioQuelca <florens.lonwe@gmail.com>
2022-11-15 23:58:14 -0400

    avances

--- app.js
+++ app.js
@@ -4,6 +4,50 @@ const express = require('express')
 const port = 9000
 const app = express()
 
+//? HABILITAR RECIBIR FORMATO JSON
+app.use(express.json())
+    //? peticion -> request ->req
+    //? respuesta -> response -> res
+
+
+const todoDB = []
+    //? TODO
+    //  {
+    //      id:5,
+    //      title:"practicar expres",
+    //      despcription:"ver videos de apoyo y crar otra API",
+    //      is_completed :false
+    //  }
+let id = 1
+
+
+app.get('/', (req, res) => {
+        res.json({
+            message: 'OK'
+        })
+    })
+    //? /todo /todo/:id
+    //? Rutas de TODO's
+app.get('/todo', (req, res) => {
+        res.status(200).json(todoDB)
+    })
+    //siempre devolver algo porque genera un bucle 
+app.post('/todo', (req, res) => {
+    const { title, description } = req.body
+    if (title && description) {
+        const newTodo = {
+            id: id++,
+            title,
+            description,
+            is_completed: false
+        }
+        todoDB.push(newTodo)
+        res.status(200).json(newTodo)
+    } else {
+        res.status(400).json({ message: "Invalid Data" })
+    }
+})
+
 
 
 app.listen(port, () => {
